0bede1e91a642e8051facbc686ed634c022945b8
pavlomarii <pmarii@cybercraftinc.com>
2021-09-09 15:08:42 +0300

    Added Edit User Functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
 
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
@@ -14,7 +18,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated!"
+      redirect_to articles_path
+    else
+      render "edit"
+    end
+  end
+
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
