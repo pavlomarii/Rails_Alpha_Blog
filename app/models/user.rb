72b2982cfa3ad9fe4ad82e9276bde6f3423ecf1f
pavlomarii <pmarii@cybercraftinc.com>
2021-09-09 12:49:43 +0300

    Added association between user and article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  has_many :articles
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
