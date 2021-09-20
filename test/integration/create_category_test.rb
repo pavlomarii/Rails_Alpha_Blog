c92b36a12f3befcfeb10e3b4c768677f60468bff
pavlomarii <pmarii@cybercraftinc.com>
2021-09-20 15:35:37 +0300

    Added second integration test

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+
   test "get new category form and create category" do
     get "/categories/new"
     assert_response :success
@@ -11,5 +12,16 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :success
     assert_match "Sports", response.body
+  end
+
+  test "get new category form and reject invalid category" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: "" } }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select "h4.alert-heading"
   end
 end
