4d8373fe303964e8349e133daca44cbb61628a6f
pavlomarii <pmarii@cybercraftinc.com>
2021-09-20 16:38:40 +0300

    Added require_admin and tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,18 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: %i[index show]
+
   def new
     @category = Category.new
   end
+
   def show
     @category = Category.find(params[:id])
   end
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
+
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -19,7 +24,15 @@ class CategoriesController < ApplicationController
   end
 
   private
+
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    unless logged_in? && current_user.admin?
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 end
--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(username: "pavlomarii", email: "pavlomarii@gmail.com", password: "password", admin: true)
+    sign_in_as(@admin_user)
+  end
 
   test "get new category form and create category" do
     get "/categories/new"
